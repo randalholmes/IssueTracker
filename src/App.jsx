3e9ae953d3df046cfe15638fb9af3c13e85cd25a
Randal Holmes <randal.holmes@gmail.com>
2020-06-29 10:32:07 -0700

    Mocked data for issues and added border styling to issue table.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,101 @@
 
+class IssueFilter extends React.Component {
+
+  render() {
+    return (
+      <div>This is placeholder for the issue Filter.</div>
+    );
+  }
+}
+
+class IssueRow extends React.Component {
+
+  render() {
+    const issue = this.props.issue;
+    return (
+      <tr>
+        <td>{issue.id}</td>
+        <td>{issue.status}</td>
+        <td>{issue.owner}</td>
+        <td>{issue.created.toDateString()}</td>
+        <td>{issue.effort}</td>
+        <td>{issue.completionDate ?
+          issue.completionDate.toDateString() : ''}</td>
+        <td>{issue.title}</td>
+      </tr>
+    );
+  }
+}
+
+
+class IssueTable extends React.Component {
+
+  render() {
+    const issueRows = this.props.issues.map(issue => <IssueRow
+    key={issue.Id} issue={issue} />);
+
+    return(
+      <table className="bordered-table">
+        <thead>
+          <th>Id</th>
+          <th>Status</th>
+          <th>Owner</th>
+          <th>Created</th>
+          <th>Effort</th>
+          <th>Completion Date</th>
+          <th>Title</th>
+        </thead>
+        <tbody>{issueRows}</tbody>
+      </table>
+    );
+  }
+}
+
+
+class IssueAdd extends React.Component {
+
+  render() {
+    return (
+      <div>This is a placeholder for an issue Add form</div>
+    );
+  }
+}
+
+
+const issues = [
+  {
+    id: 1, status: 'open', owner: 'Raven',
+    created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+    title: 'Error in console when clicking Add.'
+  },
+  {
+    id: 2, status: 'Assigned', owner: 'Eddie',
+    created: new Date('2016-08-16'), effort: 14,
+    completionDate: new Date('2016-08-30'),
+    title: 'Missing bottom border on panel.'
+  }
+];
+
+
+class IssueList extends React.Component {
+
+  render () {
+    return (
+      <div>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable issues={issues} />
+        <hr />
+        <IssueAdd />
+      </div>
+    );
+  }
+}
+
+
+
+
+
 const contentNode = document.getElementById('contents');
-const component = <h1>Hello World</h1>;
-ReactDOM.render(component, contentNode);
+ReactDOM.render(<IssueList />, contentNode);
