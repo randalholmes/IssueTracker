1b1cde65387f4a09acc120a3c40a268b7dd11842
Randal Holmes <randal.holmes@gmail.com>
2020-07-06 11:42:06 -0700

    Added fetch api to loadData function of the IssueList component.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 //
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.static('static'));
+app.use(bodyParser.json());
 
 const issues = [
   {
@@ -23,6 +25,19 @@ app.get('/api/issues', (req, res) => {
   const metadata = {total_count: issues.length};
   res.json({_metadata: metadata, records: issues});
 });
+
+app.post('/api/issues', (req, res) => {
+  const newIssue = req.body;
+  newIssue.id = issues.length + 1;
+  newIssue.created = new Date();
+
+  if (!newIssue.status)
+    newIssue.status = 'New';
+
+  issues.push(newIssue);
+
+  res.json(newIssue)
+})
 
 app.listen(3000, () => {
   console.log('App started on port 3000.');
