4bf6cc765f91311a2d1d9fbc93dc89b3abf96c9a
Randal Holmes <randal.holmes@gmail.com>
2020-07-09 10:00:52 -0700

    updated code to use async/await style syntax.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-//
+/*
+*    Node/Express Server for Issue Tracking System
+*/
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,20 +9,6 @@ const MongoClient = require('mongodb').MongoClient;
 const app = express();
 app.use(express.static('static'));
 app.use(bodyParser.json());
-
-const issues = [
-  {
-    id: 1, status: 'open', owner: 'Raven',
-    created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
-    title: 'Error in console when clicking Add.'
-  },
-  {
-    id: 2, status: 'Assigned', owner: 'Eddie',
-    created: new Date('2016-08-16'), effort: 14,
-    completionDate: new Date('2016-08-30'),
-    title: 'Missing bottom border on panel.'
-  }
-];
 
 const validIssueStatus = {
   New: true,
@@ -41,27 +29,40 @@ const issueFieldTypes = {
   title: 'required'
 };
 
+// Check that the fields in issue match fields in issueFieldTypes and
+// that required fields have values.
 function validateIssue(issue) {
-  // check that the fields in issue match fields in issueFieldTypes and
-  // that required fields have values.
-  for (const field in issue) {
-    const type = issueFieldTypes[field];
-    if (!type) {
-      delete issue[field];
-    } else if (type === 'required' && !issue[field]) {
-      return `${field} is required.`;
+  for (const type in issue) {
+    const value = issueFieldTypes[type];
+    if (!value) {
+      delete issue[type];// This type is not valid.
+    } else if (value === 'required' && !issue[type]) {
+      return `${type} is required.`;
     }
   }
 
-  // check that issue.status is a valid value
+  // Check that issue.status is a valid value
   if (!validIssueStatus[issue.status])
-    return '${issue.status} is not a valid status.';
+    return `${issue.status} is not a valid status.`;
 
   return null;
 }
 
+// Returns all issue records
+app.get('/api/issues', async (req,res) => {
+  try {
+    const issues = await db.collection('issues').find({}).toArray();
+    const metadata = {total_count: issues.length};
+    res.json({_metadata: metadata, records:issues});
+  } catch(err) {
+    console.log(error);
+    res.status(500).json({message: `Internal Server Error: ${error}`});
+  }
+});
 
-// returns all issue records
+
+/*
+// Returns all issue records
 app.get('/api/issues', (req, res) => {
   db.collection('issues').find().toArray()
   .then(issues => {
@@ -73,10 +74,44 @@ app.get('/api/issues', (req, res) => {
     res.status(500).json({message: `Internal Server Error: ${error}`});
   });
 });
+*/
 
 
-// adds a new issue record if data is valid.
-// returns the completed record for the new issue or error when
+// Adds a new issue record if data is valid.
+// Returns the completed record for the new issue or error when
+// data is invalid.
+app.post('/api/issues', async (req, res) => {
+  const newIssue = req.body;
+  newIssue.created = new Date();
+
+  if (!newIssue.status)
+    newIssue.status = 'New';
+
+  const err = validateIssue(newIssue);
+  if (err){
+    res.status(422).json({message: `Invalid request: ${err}`});
+    return;
+  }
+
+  try {
+    const result = await db.collection('issues').insertOne(newIssue);
+    const _newIssue = await db.collection('issues')
+                    .find({_id: result.insertedId}).limit(1).next();
+
+    res.json(_newIssue);
+
+  } catch(error) {
+    console.log(error);
+    res.status(500).json({message: `Internal Server Error: ${error}`});
+  }
+});
+
+
+
+
+/*
+// Adds a new issue record if data is valid.
+// Returns the completed record for the new issue or error when
 // data is invalid.
 app.post('/api/issues', (req, res) => {
   const newIssue = req.body;
@@ -107,8 +142,10 @@ app.post('/api/issues', (req, res) => {
     res.status(500).json({message: `Internal Server Error: ${error}`});
   });
 });
+*/
 
 
+/****  Connect to Database Server and Network Socket ****/
 const client = new MongoClient('mongodb://localhost',
                   { useUnifiedTopology: true });
 
